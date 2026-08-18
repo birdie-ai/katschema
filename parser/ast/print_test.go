9ea39108804eb888ba4e77f306497f654af84d53
i4k <t.nateldemoura@gmail.com>
2026-08-18 12:37:59 +0200

    feat: add sum type support in the syntactic AST.
    Signed-off-by: i4k <t.nateldemoura@gmail.com>

--- parser/ast/print_test.go
+++ parser/ast/print_test.go
@@ -278,6 +278,46 @@ func TestPrint(t *testing.T) {
 			},
 			want: `(int,x<(x+1)*2)`,
 		},
+		{
+			name: "simple sum type",
+			root: func(a *Tree) NodeID {
+				return a.AddSum(
+					a.AddSchema(a.AddName("int", z), nil, z),
+					a.AddSchema(a.AddName("string", z), nil, z),
+					z)
+			},
+			want: `(int)|(string)`,
+		},
+		{
+			name: "sum type with literal schema values",
+			root: func(a *Tree) NodeID {
+				return a.AddSum(
+					a.AddSchema(
+						a.AddObject([]Field{
+							{
+								Name:  a.AddText("str"),
+								Value: a.AddString("test", z),
+							},
+						}, z), nil, z),
+					a.AddSchema(a.AddName("string", z), nil, z),
+					z,
+				)
+			},
+			want: `({"str":"test"})|(string)`,
+		},
+		{
+			name: "sum: chaining more than 2 types",
+			root: func(a *Tree) NodeID {
+				return a.AddSum(
+					a.AddSchema(a.AddName("int", z), nil, z),
+					a.AddSum(
+						a.AddSchema(a.AddName("float", z), nil, z),
+						a.AddSchema(a.AddName("string", z), nil, z),
+						z),
+					z)
+			},
+			want: `(int)|(float)|(string)`,
+		},
 	} {
 		t.Run(tc.name, func(t *testing.T) {
 			tree := New()
